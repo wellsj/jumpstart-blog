165b7431aef1e15e567bed0eb5aa961080d64361
Jason Wells <jandr.solutions@gmail.com>
2015-06-13 23:45:15 +1200

    completed form-based workflow

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
 	def article_params
 		params.require(:article).permit(:title, :body)
-	end
+	end 	
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,3 +1,4 @@
 Blogger::Application.routes.draw do
+  root 'articles#index'
   resources :articles
 end
